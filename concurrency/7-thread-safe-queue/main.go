c5920bdb18825ecc30e53b7a3d1230de7036d9ca
Jiten <jpalaparthi@gmail.com>
2026-06-26 08:09:40 +0530

    add more examples

--- concurrency/7-thread-safe-queue/main.go
+++ concurrency/7-thread-safe-queue/main.go
@@ -3,63 +3,94 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand/v2"
+	"sync"
+	"time"
 )
 
-// is it a concurrent quque? it is not , thread safe, how to make threadsafe queue
 func main() {
 
-	queue := NewQueue(10) //  0
-	queue.Enqueue(20)     //  1
-	queue.Enqueue(30)     //  2
+	queue := NewQueue(10)
+	// fmt.Println("CIndex:", queue.CIndex, "Data:", queue.Data)
+	// queue.Enqueuq(20)
+	// fmt.Println("CIndex:", queue.CIndex, "Data:", queue.Data)
+	// queue.Enqueuq(30)
+	// fmt.Println("CIndex:", queue.CIndex, "Data:", queue.Data)
+	// queue.Enqueuq(40)
+	// fmt.Println("CIndex:", queue.CIndex, "Data:", queue.Data)
 
-	println("Current Index:", queue.CIndex)
+	// item := queue.Dequeue()
 
-	item := queue.Dequeue()
-	fmt.Println("Current Index:", queue.CIndex, "Item:", item)
+	// fmt.Println("CIndex:", queue.CIndex, "Item:", item, "Data:", queue.Data)
 
-	item = queue.Dequeue()
-	fmt.Println("Current Index:", queue.CIndex, "Item:", item)
+	// item = queue.Dequeue()
 
-	item = queue.Dequeue()
-	fmt.Println("Current Index:", queue.CIndex, "Item:", item)
+	// fmt.Println("CIndex:", queue.CIndex, "Item:", item, "Data:", queue.Data)
 
-	item = queue.Dequeue()
-	fmt.Println("Current Index:", queue.CIndex, "Item:", item)
+	// item = queue.Dequeue()
 
-	item = queue.Dequeue()
-	fmt.Println("Current Index:", queue.CIndex, "Item:", item)
+	// fmt.Println("CIndex:", queue.CIndex, "Item:", item, "Data:", queue.Data)
 
-	queue.Enqueue(40) //  2
+	wg := new(sync.WaitGroup)
 
-	item = queue.Dequeue()
-	fmt.Println("Current Index:", queue.CIndex, "Item:", item)
+	wg.Add(1)
+	go func() {
+		for i := range 10 {
+			time.Sleep(time.Millisecond * time.Duration(100+rand.IntN(100)))
+			fmt.Println("Enqueee>>") //"Data:", queue.Data)
+			queue.Enqueuq((i + 1) * 10)
+		}
+		wg.Done()
+	}()
 
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		for range 20 {
+			time.Sleep(time.Millisecond * time.Duration(100+rand.IntN(100)))
+			fmt.Println("Dequee", "Item:", queue.Dequeue())
+		}
+		wg.Done()
+	}(wg)
+
+	wg.Wait()
+	fmt.Println("Dequee", "CIndex:", queue.CIndex, "Item:", queue.Dequeue(), "Data:", queue.Data)
+	fmt.Println("Dequee", "CIndex:", queue.CIndex, "Item:", queue.Dequeue(), "Data:", queue.Data)
 }
 
 type Queue struct {
 	Data   []any
+	mu     *sync.Mutex
 	CIndex int
 }
 
 func NewQueue(d any) *Queue {
 	queue := append([]any{}, d)
-	return &Queue{queue, 0}
+	return &Queue{queue, new(sync.Mutex), 0}
 }
 
-func (q *Queue) Enqueue(item any) error {
+func (q *Queue) Enqueuq(item any) error {
 	if q == nil || item == nil {
-		return errors.New("nil queue or nil item")
+		return errors.New("nil queue or item to insert")
 	}
+	q.mu.Lock()
 	q.Data = append(q.Data, item)
-	//q.CIndex = q.CIndex + 1
+	q.mu.Unlock()
 	return nil
 }
 
 func (q *Queue) Dequeue() any {
-	item := q.Data[q.CIndex]
-	q.Data[q.CIndex] = nil
-	if q.CIndex < len(q.Data)-1 {
-		q.CIndex = q.CIndex + 1
+	if len(q.Data) > q.CIndex {
+		q.mu.Lock()
+		item := q.Data[q.CIndex]
+		q.Data[q.CIndex] = nil
+		q.CIndex++
+		q.mu.Unlock()
+		return item
 	}
-	return item
+	return nil
 }
+
+//|10| 20 |30 |40 |50 | 60|
+
+// go uses dlv debugger
+// c++ and rust used GDB and LLDB
---
+++ concurrency/8-thread-safe-queue-another-approach/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	queue := NewQueue(10)
+
+	wg := new(sync.WaitGroup)
+
+	wg.Add(1)
+	go func() {
+		for i := range 10 {
+			time.Sleep(time.Millisecond * time.Duration(100+rand.IntN(100)))
+			fmt.Println("Enqueee>>") //"Data:", queue.Data)
+			queue.Enqueuq((i + 1) * 10)
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		for range 20 {
+			time.Sleep(time.Millisecond * time.Duration(100+rand.IntN(100)))
+			fmt.Println("Dequee", "Item:", queue.Dequeue())
+		}
+		wg.Done()
+	}(wg)
+
+	wg.Wait()
+	fmt.Println("Dequee", "Item:", queue.Dequeue(), "Data:", queue.Data)
+	fmt.Println("Dequee", "Item:", queue.Dequeue(), "Data:", queue.Data)
+}
+
+type Queue struct {
+	Data []any
+	mu   *sync.Mutex
+}
+
+func NewQueue(d any) *Queue {
+	queue := append([]any{}, d)
+	return &Queue{queue, new(sync.Mutex)}
+}
+
+func (q *Queue) Enqueuq(item any) error {
+	if q == nil || item == nil {
+		return errors.New("nil queue or item to insert")
+	}
+	q.mu.Lock()
+	q.Data = append(q.Data, item)
+	q.mu.Unlock()
+	return nil
+}
+
+func (q *Queue) Dequeue() any {
+	var item any
+	if q != nil {
+		q.mu.Lock()
+		if len(q.Data) >= 1 {
+			item = q.Data[0]
+			q.Data = append([]any{}, q.Data[1:]...)
+		}
+		q.mu.Unlock()
+	}
+	return item
+}
+
+//|10| 20 |30 |40 |50 | 60|
+
+// go uses dlv debugger
+// c++ and rust used GDB and LLDB
